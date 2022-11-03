2c1f6578dbb852e297496a122c41b143cbb16990
Artem <artem-agayan@mail.ru>
2022-11-03 22:59:53 +0300

    v 3.0

--- script.js
+++ script.js
@@ -3,65 +3,33 @@ const swiper = new Swiper('.swiper', {
         slidesPerView: 4,
         spaceBetween: 30,
     // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
-/*   const swiper1 = new Swiper('.swiper1', {
-    // Default parameters
-       slidesPerView: 4,
-       spaceBetween: 30,
-    // Optional parameters
         direction: 'horizontal',
         loop: true,
     // Navigation arrows
         navigation: {
-            nextEl: '.certificates__swiper-button-next',
-            prevEl: '.certificates__swiper-button-prev',
+          nextEl: '.swiper-button-next1',
+          prevEl: '.swiper-button-prev1',
         },
- }); */
+  });
 
- const swiper3 = new Swiper('.swiper3', {
-    // Default parameters
-       slidesPerView: 2,
-       spaceBetween: 30,
-   // Optional parameters
-   direction: 'horizontal',
-   loop: true,
- 
+ const swiper2 = new Swiper('.swiper2', {
+      slidesPerView: 4,
    // If we need pagination
-   pagination: {
-     el: '.swiper-pagination',
-   },
- 
-   // Navigation arrows
-   navigation: {
-     nextEl: '.swiper-button-next3',
-     prevEl: '.swiper-button-prev3',
-   },
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-   },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      nested: true,
  });
-
+ const swiper3 = new Swiper('.swiper3', {
+     slidesPerView: 2,
+     spaceBetween: 30,
+     navigation: {
+       nextEl: '.swiper-button-next3',
+       prevEl: '.swiper-button-prev3',
+     },
+     loop: true
+});
 ///////////////////////////////
 ///INPUT MASKS
 ///////////////////////////////
@@ -78,3 +46,20 @@ let im2 = new Inputmask("+7 (999)-999-99-99", {
 im.mask(selector1);
 im.mask(selector2);
 im2.mask(selector3);
+
+/////////////
+//burger-menu
+/////////////
+
+const burger = document.querySelector('.burger__menu');
+const menu = document.querySelector('.header__nav');
+
+function openMenu(){
+  menu.style.display= 'flex'
+  document.querySelector('body').style.overflow = 'hidden'
+};
+
+function closeMenu(){
+  menu.style.display= 'none'
+  document.querySelector('body').style.overflow = 'scroll'
+};
